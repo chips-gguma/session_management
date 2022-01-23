e574be52b94b73879c5016399915cdec425432e5
chips-gguma <ljygguma@gmail.com>
2022-01-23 23:59:33 +0900

    sessionCount 작성

--- server/login-session-management/src/main/java/com/sp/fc/web/controller/SessionController.java
+++ server/login-session-management/src/main/java/com/sp/fc/web/controller/SessionController.java
@@ -3,7 +3,10 @@ package com.sp.fc.web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.stream.Collectors;
 
 @Controller
 public class SessionController {
@@ -12,7 +15,10 @@ public class SessionController {
     private SessionRegistry sessionRegistry;
 
     @GetMapping("/sessions")
-    public String sessions() {
+    public String sessions(Model model) { // sessionList page 에 넘김
+        // userSession 에 principal 를 받아서 mapping
+        model.addAttribute("sessionList", sessionRegistry.getAllPrincipals().stream().map(p->UserSession.builder()
+                .build()).collect(Collectors.toList()));
         return "/sessionList";
     }
 

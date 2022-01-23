4221d9d9f688f88dc288f67980ec98deeca3a20b
chips-gguma <ljygguma@gmail.com>
2022-01-23 23:52:39 +0900

    UserSession 필드 작성

--- server/login-session-management/src/main/java/com/sp/fc/web/controller/UserSession.java
+++ server/login-session-management/src/main/java/com/sp/fc/web/controller/UserSession.java
@@ -1,4 +1,17 @@
 package com.sp.fc.web.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserSession {
+
+    private String username;
+    private List<SessionInfo> sessions;
+
 }

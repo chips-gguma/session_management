55b11d226c42eeafba8486cef4ac856e2f2a64df
chips-gguma <ljygguma@gmail.com>
2022-01-24 23:57:48 +0900

    GetMapping 으로 session-expired

--- server/login-session-management/src/main/java/com/sp/fc/web/controller/SessionController.java
+++ server/login-session-management/src/main/java/com/sp/fc/web/controller/SessionController.java
@@ -21,15 +21,16 @@ public class SessionController {
     @GetMapping("/sessions")
     public String sessions(Model model) { // sessionList page 에 넘김
         // userSession 에 principal 를 받아서 mapping
-        model.addAttribute("sessionList", sessionRegistry.getAllPrincipals().stream().map(p->UserSession.builder()
-                .username(((SpUser)p).getUsername()) // SpUser 에서 user 이름을 받음
-                .sessions(sessionRegistry.getAllSessions(p, false).stream().map(s-> // pricipal 로 getAllsessions 만료된 것 말고 살아있는 session 만 가져옴
+        model.addAttribute("sessionList",
+                sessionRegistry.getAllPrincipals().stream().map(p->UserSession.builder()
+                    .username(((SpUser)p).getUsername()) // SpUser 에서 user 이름을 받음
+                    .sessions(sessionRegistry.getAllSessions(p, false).stream().map(s-> // pricipal 로 getAllsessions 만료된 것 말고 살아있는 session 만 가져옴
                         SessionInfo.builder()
                                 .sessionId(s.getSessionId())
                                 .time(s.getLastRequest())
                                 .build())
                         .collect(Collectors.toList()))
-                .build()).collect(Collectors.toList())); // UserSession 에 sessions 리스트가 주입 됨
+                        .build()).collect(Collectors.toList())); // UserSession 에 sessions 리스트가 주입 됨
         return "/sessionList";
     }
 
@@ -43,6 +44,7 @@ public class SessionController {
         return "redirect:/sessions"; // 살아있는 session만 나타남
     }
 
+    @GetMapping("/sessions-expired")
     public String sessionExpired() {
         return "/sessionExpired";
     }
